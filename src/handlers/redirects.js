8f4c1ea8d1dbd76dac0cdc9a01dd38868197cac2
StarlightWT <StarlightWT@protonmail.com>
2023-09-28 13:47:36 +0200

    reworked file system to make it more organized with growing amount of views

--- src/handlers/redirects.js
+++ src/handlers/redirects.js
@@ -1,17 +1,17 @@
 function showDashboard(win) {
-    win.loadFile("./views/dashboard.html").then(() => {
+    win.loadFile("./views/dashboard/dashboard.html").then(() => {
       win.show();
     });
 }
 function showGames(win) {
-    win.loadFile("./views/games.html").then(() => {
+    win.loadFile("./views/games/games.html").then(() => {
       win.show();
     });
 }
 
 //Casino redirects
 function blackJack(win){
-  win.loadFile("./views/casino/blackjack.html").then(() => {
+  win.loadFile("./views/blackjack/blackjack.html").then(() => {
     win.show();
   })
 }
