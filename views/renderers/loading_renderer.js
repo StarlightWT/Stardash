e7aa65caabf0b3044de42f3451a6e6bc1d5ebc52
StarlightWT <StarlightWT@protonmail.com>
2023-10-04 21:06:21 +0200

    Made loading page prettier

--- views/renderers/loading_renderer.js
+++ views/renderers/loading_renderer.js
@@ -1,4 +1,22 @@
 setInterval(async () => {
 	if ((await window.electronAPI.loaded()) == 1)
 		window.electronAPI.redirect("home");
+
+	switch (i) {
+		case 0:
+			loadingText.innerHTML = "Loading.";
+			i++;
+			break;
+		case 1:
+			loadingText.innerHTML = "Loading..";
+			i++;
+			break;
+		case 2:
+			loadingText.innerHTML = "Loading...";
+			i = 0;
+			break;
+	}
 }, 1000);
+
+const loadingText = document.getElementById("loadingText");
+var i = 0;
